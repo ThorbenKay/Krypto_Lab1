406941b188ae17322f93d087dbef02dff818b2f1
ThorbenKay <thorben.kay@haw-hamburg.de>
2018-04-09 22:49:43 +0200

    simplified structure, RSA-Straight-forward working (variable Keywordlength not tested)

--- Krypto_Lab1/Krypto_Lab1.h
+++ Krypto_Lab1/Krypto_Lab1.h
@@ -1,8 +1,3 @@
 
-//CLINT squareAndMultiply(CLINT base_X, CLINT exponent_k);
 
-bool startRSAstraightForward(char sendedMessage, int keyLength);
-
-void keyGeneration(int keyLength, CLINT privateKey_d, CLINT publicKey_e, CLINT publicKey_n);
-void encrypt(CLINT chiffre, char sendedMessage, CLINT publicKey_e, CLINT publicKey_n);
-void decrypt(char recievedMessage, CLINT chiffre, CLINT publicKey_e, CLINT publicKey_n, CLINT privateKey_d);
+void squareAndMultiply(CLINT m, CLINT e, CLINT n, CLINT *chiffre);
