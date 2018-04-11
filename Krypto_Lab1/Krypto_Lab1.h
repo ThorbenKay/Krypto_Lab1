ed8c259f3004a1422606a74eba6879baf8bbe03a
ThorbenKay <thorben.kay@haw-hamburg.de>
2018-04-11 14:29:15 +0200

    Perpared Stucture for CRT implementation

--- Krypto_Lab1/Krypto_Lab1.h
+++ Krypto_Lab1/Krypto_Lab1.h
@@ -1,3 +1,5 @@
 
 
 void squareAndMultiply(CLINT m, CLINT e, CLINT n, CLINT *chiffre);
+void startRSAstraightForward(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec);
+void startRSAchineseRemainderTheorem(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec);
