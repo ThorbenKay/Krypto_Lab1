e1d9a1d6b5539cbe7b6bcd712804d2ff4607e518
ThorbenKay <thorben.kay@haw-hamburg.de>
2018-04-14 22:03:41 +0200

    first CRT Implementation, seems to run for 256 Bit so far

--- Krypto_Lab1/Krypto_Lab1.h
+++ Krypto_Lab1/Krypto_Lab1.h
@@ -1,5 +1,6 @@
 
 
-void squareAndMultiply(CLINT m, CLINT e, CLINT n, CLINT *chiffre);
+void squareAndMultiply(CLINT base, CLINT exponent, CLINT n, CLINT *result);
+void chineseReminderTheorem(CLINT primeNumber_P, CLINT primeNumber_q, CLINT message, CLINT privateKey_d, CLINT publicKey_n, CLINT *sig);
 void startRSAstraightForward(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec);
 void startRSAchineseRemainderTheorem(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec);
