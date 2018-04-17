f822b4b7b5924d78286635f6898939c45a405c94
ThorbenKay <thorben.kay@haw-hamburg.de>
2018-04-17 21:16:18 +0200

    corrected CRT placement. CRT is only in decryption now. running for variable Keylength now . Ready for Hardware-Test

--- Krypto_Lab1/Krypto_Lab1.h
+++ Krypto_Lab1/Krypto_Lab1.h
@@ -1,6 +1,7 @@
+typedef unsigned short clint;
+typedef clint CLINT[CLINTMAXDIGIT + 1];
 
-
-void squareAndMultiply(CLINT base, CLINT exponent, CLINT n, CLINT *result);
-void chineseReminderTheorem(CLINT primeNumber_P, CLINT primeNumber_q, CLINT message, CLINT privateKey_d, CLINT publicKey_n, CLINT *sig);
-void startRSAstraightForward(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec);
-void startRSAchineseRemainderTheorem(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec);
+void squareAndMultiply(CLINT base, CLINT exponent, CLINT n, CLINT *result, int keyLength);
+void chineseReminderTheorem(CLINT primeNumber_P, CLINT primeNumber_q, CLINT message, CLINT privateKey_d, CLINT publicKey_n, CLINT *sig, int keyLength);
+void startRSAstraightForward(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec, int keyLength);
+void startRSAchineseRemainderTheorem(char *primeNumber_P_dec, char *primeNumber_q_dec, char *publicKey_e_dec, char *privateKey_d_dec, int keyLength);
